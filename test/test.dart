8f140c9c89cdc1f4ed10030450bc11545bc4c524
Tyler <scottt2@uw.edu>
2019-10-12 10:11:10 -0600

    Add another traindown example to parse

--- test/test.dart
+++ test/test.dart
@@ -4,8 +4,15 @@ import "../lib/src/scanner.dart";
 
 void main() {
   var scanner = Scanner("./test.traindown");
+  var scanner1 = Scanner("./test1.traindown");
+
   var parser = Parser(scanner);
   var presenter = ConsolePresenter(parser);
+
+  parser.parse();
+  print(presenter.call());
+
+  parser.scanner = scanner1;
   parser.parse();
   print(presenter.call());
 }
