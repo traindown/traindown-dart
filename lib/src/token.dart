27248b7e0d35c72e606b7936cc3bf4e32ce80420
Tyler <scottt2@uw.edu>
2019-10-19 14:29:54 -0600

    Prune unused tokens

--- lib/src/token.dart
+++ lib/src/token.dart
@@ -1,16 +1,12 @@
 class Token {
   static const COLON = "Colon";
-  static const DATE = "Date";
   static const EOF = "EOF";
   static const IDENT = "Identifier";
   static const ILLEGAL = "Illegal";
   static const LINEBREAK = "Linebreak";
   static const POUND = "Pound";
-  static const REP = "Rep";
   static const SEMICOLON = "Semicolon";
-  static const SET = "Set";
   static const STAR = "Star";
-  static const TIME = "Time";
   static const UNIT = "Unit";
   static const WHITESPACE = "Whitespace";
 }
