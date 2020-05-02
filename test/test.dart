2d9efea86d38d3718c1f415412fcc9d7de9450c3
Tyler <scottt2@uw.edu>
2020-05-02 08:02:34 -0600

    Clean up test

--- test/test.dart
+++ test/test.dart
@@ -3,39 +3,21 @@ import "package:traindown/src/presenters.dart";
 import "package:traindown/src/scanner.dart";
 
 void main() {
-  var scanner = Scanner(filename: "./test.traindown");
-  var scanner1 = Scanner(filename: "./test1.traindown");
-  var scanner2 = Scanner(string: """
-    # Date: 20191021
-    Squat: 500; 550 2r; 600 3r 3s;""");
-  var scanner3 = Scanner(filename: "./test2.traindown");
+  var scanner = Scanner(string: """
+    @ 20191021
+    # unit: lbs
+    Squat: 500 550 2r 600 3r 3s""");
+  var scanner1 = Scanner(filename: "./test.traindown");
 
   var parser = Parser(scanner);
   var presenter = ConsolePresenter(parser);
 
-  /*
   parser.parse();
-  print(parser.movements);
   print(presenter.call());
+
+  print("\n\n---\n\n");
 
   parser.scanner = scanner1;
   parser.parse();
   print(presenter.call());
-
-  parser.scanner = scanner2;
-  parser.parse();
-  print(presenter.call());
-  */
-
-  parser.scanner = scanner3;
-  parser.parse();
-  print(presenter.call());
-
-  /*
-  while (!scanner3.eof) {
-    var tokenLiteral = scanner3.scan();
-
-    print(tokenLiteral);
-  }
-  */
 }
