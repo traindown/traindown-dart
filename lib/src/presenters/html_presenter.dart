96d68baeba257d739884c47e02cdd9fcab39734b
Tyler <scottt2@uw.edu>
2020-05-08 20:02:06 -0600

    Add some style

--- lib/src/presenters/html_presenter.dart
+++ lib/src/presenters/html_presenter.dart
@@ -1,6 +1,9 @@
 import "package:traindown/src/parser.dart";
 import "package:traindown/src/presenter.dart";
 
+final String style =
+    "html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0;overflow:visible}pre{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,samp{font-family:monospace,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,input{overflow:visible}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:1px dotted ButtonText}fieldset{padding:.35em .75em .625em}legend{box-sizing:border-box;color:inherit;display:table;max-width:100%;padding:0;white-space:normal}progress{vertical-align:baseline}textarea{overflow:auto}[type=checkbox],[type=radio]{box-sizing:border-box;padding:0}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}details{display:block}summary{display:list-item}template{display:none}[hidden]{display:none} *,*:after,*:before{box-sizing:inherit}html{box-sizing:border-box;font-size:62.5%}body{margin:1em auto;max-width:960px;color:#606c76;font-family:'Roboto','Helvetica Neue','Helvetica','Arial',sans-serif;font-size:1.6em;font-weight:300;letter-spacing:.01em;line-height:1.6;padding:.5rem}hr{border:0;border-top:.1rem solid #f4f5f6;margin:3rem 0}hr.flat-top{margin-top:1rem}pre{background:#f4f5f6;border-left:.3rem solid #666;padding:1.25rem 1.5rem;overflow-y:hidden;white-space:pre-wrap}pre.wrong{border-left:.3rem solid red;margin-bottom:0}h1,h2,h3,h4,h5,h6{font-weight:300;letter-spacing:-.1rem;margin-bottom:2rem;margin-top:0}h1{font-size:4.6rem;line-height:1;margin-bottom:.5rem}h2{font-size:3.6rem;line-height:1.25}h3{font-size:2.8rem;line-height:1.3}h4{font-size:2rem}";
+
 class HtmlPresenter extends Presenter {
   HtmlPresenter(Parser parser) : super(parser);
 
@@ -8,6 +11,7 @@ class HtmlPresenter extends Presenter {
   StringBuffer initString() {
     StringBuffer string = StringBuffer();
 
+    string.write("<style>$style</style>");
     string.write("<h1>Training Session on $occurred</h1>");
 
     return string;
