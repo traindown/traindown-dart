5392b12142ac026e5dc7ff2763f7761c9b6e43b2
Tyler <scottt2@uw.edu>
2020-05-21 18:15:49 -0600

    Remove CLI from base library

--- lib/traindown.dart
+++ lib/traindown.dart
@@ -2,3 +2,6 @@ export "src/formatter.dart";
 export "src/scanner.dart";
 export "src/parser.dart";
 export "src/presenter.dart";
+export "src/presenters/console_presenter.dart";
+export "src/presenters/html_presenter.dart";
+export "src/presenters/json_presenter.dart";
